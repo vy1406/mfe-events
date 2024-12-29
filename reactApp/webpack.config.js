2181789531a8174211a2a7c5f6494ad29bdc85a5
Vladimir Elisavetski <yvova88@yahoo.com>
2024-12-29 14:51:41 -0700

    wip

--- reactApp/webpack.config.js
+++ reactApp/webpack.config.js
@@ -16,12 +16,19 @@ module.exports = (env) => ({
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ]
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
